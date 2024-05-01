2d6fd150aa0391e2e28f62972fa94db471a80a3b
moises0142 <lopez.moises0142@gmail.com>
2024-05-01 02:18:18 -0400

    js works great working on css now

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-function add(a,b){
-    return +a+ +b;
-}
-
-function subtract(a,b){
-    return +a- +b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return +a/ +b;
-}
-
 let firstNum;
 let operand;
 let secondNum;
 let output;
-
+let num;
 function operate(){
     
     if(operand == ('+')){
-        result = add(firstNum,secondNum);
+        num= +firstNum+ +secondNum;
+        result = Math.round(num*1000)/1000;
         return result;
     }
 
     if(operand == ('-')){
-        result = subtract(firstNum,secondNum);
+        num= +firstNum - +secondNum;
+        result = Math.round(num*1000)/1000;
         return result;
     }
 
     if(operand == ('*')){
-        result = multiply(firstNum,secondNum);
+        num= +firstNum * +secondNum;
+        result = Math.round(num*1000)/1000;
         return result;
     }  
 
@@ -38,22 +27,28 @@ function operate(){
         if(secondNum == 0){
             return "ayooo";
         }
-        result = divide(firstNum,secondNum);
+        num= +firstNum / +secondNum;
+        result = Math.round(num*1000)/1000;
         return result;
     }
 }
 
 let body = document.querySelector("body");
+let displayHtml = document.querySelector(".displayHtml")
 let numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(populateDisplay);
 let display = document.createElement("div");
+display.classList.add('display')
 let answer = document.createElement("div");
+answer.classList.add('display')
 let clearBtn = document.querySelector('.clear');
 let operandBtn = document.querySelectorAll('.operand');
 let equalBtn = document.querySelector('.equals')
 operandBtn.forEach(operandBtnFunc);
 let i = 0;
-
+let dot = document.querySelector(".dot");
+let backspace = document.querySelector(".backSpace");
+let displayContainer;
 
 clearBtn.addEventListener("click",()=>{
     firstNum= '';
@@ -70,7 +65,8 @@ function populateDisplay(el){
     el.addEventListener("click",()=>{
         answer.textContent = '';
         display.textContent += el.textContent;
-        body.appendChild(display);
+        displayContainer = display.textContent;
+        displayHtml.appendChild(display);
     });
 }
 
@@ -103,13 +99,13 @@ function operandBtnFunc(el){
             if(display.textContent == ''){
                 secondNum=firstNum;
             }
-       
             else{
                 secondNum = display.textContent;
             } 
             if(isNaN(firstNum)){
                 firstNum=secondNum;
             }
+      
             getResult();
         }
 
@@ -130,15 +126,45 @@ function getResult(){
     //secondDisplayContainer =display.textContent;
     if(display.textContent !== ''){    
         secondNum = display.textContent;
+    }   
+    if(isNaN(secondNum)){
+        secondNum=firstNum
     }
-    
+    if(isNaN(firstNum)){
+        firstNum=secondNum
+    }
+    if(isNaN(firstNum) && isNaN(secondNum)){
+        firstNum=0;
+        secondNum=0;
+    }
     display.textContent='';       
     answer.textContent = operate();
     output = answer.textContent;
-    body.appendChild(answer);
+    displayHtml.appendChild(answer);
 
 
     console.log(operand);
     console.log(firstNum);
     console.log(secondNum);
 }
+
+backspace.addEventListener("click",()=>{
+    if(output){
+        gyatt = output.slice(0,-1);
+        answer.textContent=gyatt;
+        output=gyatt;
+    
+        displayHtml.appendChild(answer);
+    } 
+    else{   
+    gyatt = displayContainer.slice(0,-1);    
+    display.textContent=gyatt;
+    displayContainer=gyatt;
+
+    displayHtml.appendChild(display);
+    }
+});
+
+dot.addEventListener("click",()=>{
+    display.textContent +=".";
+})
